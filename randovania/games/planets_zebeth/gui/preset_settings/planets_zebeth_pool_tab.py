ca3103867a57763bd18871120cece436b976f169
UltiNaruto <zenaruto@ymail.com>
2026-03-13 03:57:28 +0100

    Planets (Zebeth): Fixed PlanetsZebethPresetPickupPool crashing when reopening

--- randovania/games/planets_zebeth/gui/preset_settings/planets_zebeth_pool_tab.py
+++ randovania/games/planets_zebeth/gui/preset_settings/planets_zebeth_pool_tab.py
@@ -8,11 +8,12 @@ from randovania.interface_common.preset_editor import PresetEditor
 
 
 class PlanetsZebethPresetPickupPool(MetroidPresetPickupPool):
-    ammo_widgets: list[QCheckBox] = []
+    ammo_widgets: list[QCheckBox]
 
     def __init__(self, editor: PresetEditor, game_description: GameDescription, window_manager: WindowManager):
         super().__init__(editor, game_description, window_manager)
 
+        self.ammo_widgets = []
         for ammo_def, ammo_widget in self._ammo_pickup_widgets.items():
             if ammo_def.name.endswith("Missile Tank"):
                 if ammo_widget.require_main_item_check is not None:
