982ede47c47935fb9a32d0b6c9b3b0c2c6d08bf3
dyceron <dycerontv@gmail.com>
2026-04-12 09:26:27 -0400

    Remove label on export dialog

--- randovania/games/prime_hunters/gui/generated/prime_hunters_game_export_dialog_ui.py
+++ randovania/games/prime_hunters/gui/generated/prime_hunters_game_export_dialog_ui.py
@@ -28,61 +28,56 @@ class Ui_HuntersGameExportDialog(object):
         self.gridLayout.setSpacing(6)
         self.gridLayout.setContentsMargins(11, 11, 11, 11)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.output_file_button = QPushButton(HuntersGameExportDialog)
-        self.output_file_button.setObjectName(u"output_file_button")
+        self.input_file_label = QLabel(HuntersGameExportDialog)
+        self.input_file_label.setObjectName(u"input_file_label")
 
-        self.gridLayout.addWidget(self.output_file_button, 4, 1, 1, 1)
+        self.gridLayout.addWidget(self.input_file_label, 1, 0, 1, 1)
 
-        self.auto_save_spoiler_check = QCheckBox(HuntersGameExportDialog)
-        self.auto_save_spoiler_check.setObjectName(u"auto_save_spoiler_check")
+        self.input_file_button = QPushButton(HuntersGameExportDialog)
+        self.input_file_button.setObjectName(u"input_file_button")
 
-        self.gridLayout.addWidget(self.auto_save_spoiler_check, 6, 0, 1, 1)
-
-        self.cancel_button = QPushButton(HuntersGameExportDialog)
-        self.cancel_button.setObjectName(u"cancel_button")
-
-        self.gridLayout.addWidget(self.cancel_button, 7, 1, 1, 1)
-
-        self.output_file_label = QLabel(HuntersGameExportDialog)
-        self.output_file_label.setObjectName(u"output_file_label")
-
-        self.gridLayout.addWidget(self.output_file_label, 3, 0, 1, 1)
+        self.gridLayout.addWidget(self.input_file_button, 2, 1, 1, 1)
 
         self.output_file_edit = QLineEdit(HuntersGameExportDialog)
         self.output_file_edit.setObjectName(u"output_file_edit")
 
         self.gridLayout.addWidget(self.output_file_edit, 4, 0, 1, 1)
 
+        self.cancel_button = QPushButton(HuntersGameExportDialog)
+        self.cancel_button.setObjectName(u"cancel_button")
+
+        self.gridLayout.addWidget(self.cancel_button, 6, 1, 1, 1)
+
+        self.auto_save_spoiler_check = QCheckBox(HuntersGameExportDialog)
+        self.auto_save_spoiler_check.setObjectName(u"auto_save_spoiler_check")
+
+        self.gridLayout.addWidget(self.auto_save_spoiler_check, 5, 0, 1, 1)
+
         self.accept_button = QPushButton(HuntersGameExportDialog)
         self.accept_button.setObjectName(u"accept_button")
 
-        self.gridLayout.addWidget(self.accept_button, 7, 0, 1, 1)
+        self.gridLayout.addWidget(self.accept_button, 6, 0, 1, 1)
 
-        self.input_file_label = QLabel(HuntersGameExportDialog)
-        self.input_file_label.setObjectName(u"input_file_label")
+        self.output_file_button = QPushButton(HuntersGameExportDialog)
+        self.output_file_button.setObjectName(u"output_file_button")
 
-        self.gridLayout.addWidget(self.input_file_label, 1, 0, 1, 1)
-
-        self.output_format_label = QLabel(HuntersGameExportDialog)
-        self.output_format_label.setObjectName(u"output_format_label")
-
-        self.gridLayout.addWidget(self.output_format_label, 5, 0, 1, 1)
-
-        self.description_label = QLabel(HuntersGameExportDialog)
-        self.description_label.setObjectName(u"description_label")
-        self.description_label.setWordWrap(True)
-
-        self.gridLayout.addWidget(self.description_label, 0, 0, 1, 2)
+        self.gridLayout.addWidget(self.output_file_button, 4, 1, 1, 1)
 
         self.input_file_edit = QLineEdit(HuntersGameExportDialog)
         self.input_file_edit.setObjectName(u"input_file_edit")
 
         self.gridLayout.addWidget(self.input_file_edit, 2, 0, 1, 1)
 
-        self.input_file_button = QPushButton(HuntersGameExportDialog)
-        self.input_file_button.setObjectName(u"input_file_button")
+        self.output_file_label = QLabel(HuntersGameExportDialog)
+        self.output_file_label.setObjectName(u"output_file_label")
 
-        self.gridLayout.addWidget(self.input_file_button, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.output_file_label, 3, 0, 1, 1)
+
+        self.description_label = QLabel(HuntersGameExportDialog)
+        self.description_label.setObjectName(u"description_label")
+        self.description_label.setWordWrap(True)
+
+        self.gridLayout.addWidget(self.description_label, 0, 0, 1, 2)
 
 
         self.retranslateUi(HuntersGameExportDialog)
@@ -92,17 +87,16 @@ class Ui_HuntersGameExportDialog(object):
 
     def retranslateUi(self, HuntersGameExportDialog):
         HuntersGameExportDialog.setWindowTitle(QCoreApplication.translate("HuntersGameExportDialog", u"Game Patching", None))
-        self.output_file_button.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Select File", None))
-        self.auto_save_spoiler_check.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Include a spoiler log in the same directory", None))
-        self.cancel_button.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Cancel", None))
-        self.output_file_label.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Output File", None))
-        self.output_file_edit.setPlaceholderText(QCoreApplication.translate("HuntersGameExportDialog", u"Path to where the randomized game will be outputted", None))
-        self.accept_button.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Accept", None))
         self.input_file_label.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Input File (Vanilla Prime Hunters ROM)", None))
-        self.output_format_label.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Output Format", None))
-        self.description_label.setText(QCoreApplication.translate("HuntersGameExportDialog", u"<html><head/><body><p>In order to create the randomized game, a ROM of Metroid Prime Hunters for the Nintendo DS is necessary.</p></body></html>", None))
+        self.input_file_button.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Select File", None))
+        self.output_file_edit.setPlaceholderText(QCoreApplication.translate("HuntersGameExportDialog", u"Path to where the randomized game will be outputted", None))
+        self.cancel_button.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Cancel", None))
+        self.auto_save_spoiler_check.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Include a spoiler log in the same directory", None))
+        self.accept_button.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Accept", None))
+        self.output_file_button.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Select File", None))
         self.input_file_edit.setText("")
         self.input_file_edit.setPlaceholderText(QCoreApplication.translate("HuntersGameExportDialog", u"Path to Hunters.nds", None))
-        self.input_file_button.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Select File", None))
+        self.output_file_label.setText(QCoreApplication.translate("HuntersGameExportDialog", u"Output File", None))
+        self.description_label.setText(QCoreApplication.translate("HuntersGameExportDialog", u"<html><head/><body><p>In order to create the randomized game, a ROM of Metroid Prime Hunters for the Nintendo DS is necessary.</p></body></html>", None))
     # retranslateUi
 
