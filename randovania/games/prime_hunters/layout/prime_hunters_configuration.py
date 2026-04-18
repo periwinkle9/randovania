8308b90f98570ecdd023d65f9710dee73a5db85a
dyceron <dycerontv@gmail.com>
2026-04-18 10:58:14 -0400

    Update OPHR to 0.5.0

--- randovania/games/prime_hunters/layout/prime_hunters_configuration.py
+++ randovania/games/prime_hunters/layout/prime_hunters_configuration.py
@@ -21,9 +21,9 @@ class HuntersConfiguration(BaseConfiguration):
     octoliths: HuntersOctolithConfig
     force_field_configuration: ForceFieldConfiguration
     teleporters: TeleporterConfiguration
-    missile_launcher_ammo: int = dataclasses.field(metadata={"min": 1, "max": 99, "precision": 1})
-    missile_expansion_ammo: int = dataclasses.field(metadata={"min": 1, "max": 99, "precision": 1})
-    ua_expansion_ammo: int = dataclasses.field(metadata={"min": 10, "max": 100, "precision": 10})
+    missile_launcher_ammo: int = dataclasses.field(metadata={"min": 1, "max": 1000, "precision": 1})
+    missile_expansion_ammo: int = dataclasses.field(metadata={"min": 2, "max": 1000, "precision": 2})
+    ua_expansion_ammo: int = dataclasses.field(metadata={"min": 2, "max": 1000, "precision": 2})
 
     @classmethod
     def game_enum(cls) -> RandovaniaGame:
